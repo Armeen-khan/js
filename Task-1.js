e0781d2d8fe0697fd978882b421c1435c3490f83
Armeen Isbah Khan~ <armeenisbah378@gmail.com>
2023-04-16 02:13:19 +0500

    Tasks js

--- Task-1.js
+++ Task-1.js
@@ -1,4 +1,4 @@
-//  TASK 1
+// TASK 1
 // -Calculate the zakat value, first, create a variable named "zakatPercentage" and
 // store the value of 2.5% in it (hint: 2.5% means 0.025). Next, create another variable
 // named "userInput" and take the input from the user using the prompt. Make sure
@@ -6,12 +6,9 @@
 // using a suitable method. Then, create a variable named "result" and assign its value
 // to the multiplication of the zakat percentage and user input. Finally, use an alert
 // message to display the calculated zakat value. The message should look like this:
-// "Your zakat value is xxx"
+// "Your zakat value is xxx".
 
 var zakatPercentage = 0.025
 var userinput = (prompt("Enter Your Amount"));
 var result = (zakatPercentage) * (userinput);
 alert ("Your Zakat Value is" + result);
-
-
-
